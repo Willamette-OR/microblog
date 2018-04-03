e33b5d118b16095b915a1208113e2cf976be3098
Wei <gongwei619@hotmail.com>
2018-04-02 21:54:15 -0400

    Create a simple form class for user login

---
+++ app/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    """A class for login forms"""
+
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Submit")

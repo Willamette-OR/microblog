110d06aa3f2e69f2561b4ffde6ac2e253f4a0ae8
Wei <gongwei619@hotmail.com>
2018-04-02 21:58:28 -0400

    Create a dummy view function for user login

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
@@ -13,3 +14,15 @@ def index():
              {'author': 'pp', 'post': 'xixi'}]
 
     return render_template('index.html', title=title, user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """View function for the login page"""
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form)

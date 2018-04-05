54300d332ae410fab599a01c46c82764d186fe3e
Wei <gongwei619@hotmail.com>
2018-04-04 21:00:18 -0400

    Create instances of sqlalchemy and sql migrate, and a simple user model

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from app import db
+
+
+class User(db.Model):
+    """A class for user data model"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        """String representation for user objects"""
+
+        return '<User {}>'.format(self.username)

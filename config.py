694df139ca3c22ef519e5df788c969a8211915c1
Wei <gongwei619@hotmail.com>
2018-04-04 20:55:24 -0400

    Update app configurations with variables for sqlalchemy databases

--- config.py
+++ config.py
@@ -1,7 +1,13 @@
 import os
 
 
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config(object):
     """A default config class for the app"""
 
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(base_dir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

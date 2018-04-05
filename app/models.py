729a2c850576dc4758bc31f25d8fac3585479032
Wei <gongwei619@hotmail.com>
2018-04-04 21:49:07 -0400

    Add a new data model for user post

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from app import db
 
 
@@ -8,8 +9,23 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         """String representation for user objects"""
 
         return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    """A class for post data model"""
+
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        """String representation for post objects"""
+
+        return '<Post {}>'.format(self.body)

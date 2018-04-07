c26a3fecb66afb8e4b93017265d870e19b69902c
Wei <gongwei619@hotmail.com>
2018-04-06 20:06:16 -0400

    Complete the login/logout mechanics to better handle requests depending on if the current user has already logged in

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template, redirect, url_for, flash
+from flask_login import current_user, login_user, logout_user
 from app import app
 from app.forms import LoginForm
 from app.models import User
@@ -20,12 +21,16 @@ def index():
 def login():
     """View function for the login page"""
 
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     form = LoginForm()
 
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
 
         if user is not None and user.check_password(form.password.data):
+            login_user(user)
             flash('You have logged in successfully!')
             return redirect(url_for('index'))
 
@@ -33,3 +38,12 @@ def login():
         return redirect(url_for('login'))
 
     return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+def logout():
+    """View function for user logout"""
+
+    if current_user.is_authenticated:
+        logout_user()
+    return redirect(url_for('index'))

8a376f0160d8ba63f4f8bf0dbc8e8a817be4bf3b
Wei <gongwei619@hotmail.com>
2018-04-06 21:03:41 -0400

    Add additional validators to the registration form to prevent duplicate usernames and email addresses

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
+
+
+from app.models import User
 
 
 class LoginForm(FlaskForm):
@@ -21,3 +24,17 @@ class RegistrationForm(FlaskForm):
     password_2 = PasswordField('Repeat password',
                                validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        """Validate if the username has already been taken"""
+
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self, email):
+        """Validate if the email address has already been taken"""
+
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')

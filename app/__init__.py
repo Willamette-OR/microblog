4e347720ddd12b2363a0cd5b2030f13a32190087
Wei <gongwei619@hotmail.com>
2018-04-10 20:13:18 -0400

    Create and register with the app an smtp logging handler for errors

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
+import logging
+from logging.handlers import SMTPHandler
+
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+
+
 from config import Config
 
 
@@ -14,3 +20,24 @@ login.login_view = 'login'
 
 
 from app import routes, models, errors
+
+
+if not app.debug:
+    if app.config['MAIL_SERVER']:
+        secure = None
+        if app.config['MAIL_USE_TLS']:
+            secure = ()
+
+        auth = None
+        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+
+        mail_handler = SMTPHandler(mailhost=(app.config['MAIL_SERVER'],
+                                             app.config['MAIL_PORT']),
+                                   fromaddr='no_reply@' +
+                                            app.config['MAIL_SERVER'],
+                                   toaddrs=app.config['ADMINS'],
+                                   subject='Microblog Failure',
+                                   credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)

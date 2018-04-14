85333e373997a90df09d6b2d4e82b78f09db8370
Wei <gongwei619@hotmail.com>
2018-04-14 14:05:43 -0400

    Add a form class to request user password reset emails

--- app/forms.py
+++ app/forms.py
@@ -71,3 +71,10 @@ class PostForm(FlaskForm):
 
     post = TextAreaField('Say something', validators=[Length(min=1, max=140)])
     submit = SubmitField('Submit')
+
+
+class RequestPasswordResetForm(FlaskForm):
+    """A class for requesting user password reset"""
+
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Send')

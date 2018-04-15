dfcf485555cb3f970cb0588d85da1a5e5a6db6b1
Wei <gongwei619@hotmail.com>
2018-04-15 11:45:03 -0400

    Update email utility functions to render email contents from a html template

--- app/email.py
+++ app/email.py
@@ -1,5 +1,5 @@
 from threading import Thread
-from flask import url_for
+from flask import render_template
 from flask_mail import Message
 
 
@@ -10,15 +10,17 @@ def send_password_reset_email(user, recipients, sender):
     """Send password reset email"""
 
     token = user.create_password_reset_token()
-    send_mail(recipients=recipients, sender=sender, token=token)
+    send_mail(recipients=recipients, sender=sender,
+              html_body=render_template('emails/email_html.html', token=token,
+                                        user=user))
 
 
-def send_mail(recipients, sender, token):
+def send_mail(recipients, sender, html_body):
     """Send mail using flask mail object's send method"""
 
     msg = Message(subject='Microblog: Reset Your Password',
                   recipients=recipients, sender=sender)
-    msg.html = "<a href='https://reset_password_request/{{ token }}'>Reset</a>"
+    msg.html = html_body
     Thread(target=send_async_send, args=(app, msg)).start()
 
 

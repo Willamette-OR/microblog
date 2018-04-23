a4c2bd53a31913a25a78bb546d7a541ef585935b
Wei <gongwei619@hotmail.com>
2018-04-22 21:04:14 -0400

    Fix a bug in error page rendering

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -3,14 +3,14 @@ from app import db
 from app.errors import bp
 
 
-@bp.errorhandler(404)
+@bp.app_errorhandler(404)
 def error_not_found(error):
     """An error handler to be registered for files not found errors"""
 
     return render_template('errors/error_404.html'), 404
 
 
-@bp.errorhandler(500)
+@bp.app_errorhandler(500)
 def error_internal(error):
     """An error handler to be registered for internal errors"""
 

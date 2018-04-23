94b4e7bd7647c39afc16994447058d0e98ca6687
Wei <gongwei619@hotmail.com>
2018-04-22 21:06:40 -0400

    Move the email module out of the auth sub module to the app folder

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -4,10 +4,10 @@ from flask_login import current_user, login_user, logout_user
 
 from app import db
 from app.models import User
+from app.email import send_password_reset_email
 from app.auth import bp
 from app.auth.forms import LoginForm, RegistrationForm, \
     RequestPasswordResetForm, PasswordResetForm
-from app.auth.email import send_password_reset_email
 
 
 @bp.route('/login', methods=['GET', 'POST'])

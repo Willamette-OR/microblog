7c63e6f46a507f64981abba284af7276d69f99fc
Wei <gongwei619@hotmail.com>
2018-04-26 21:16:15 -0400

    Attach a search form to flask g before all requests, and add a view function to handle GET search requests

--- app/main/routes.py
+++ app/main/routes.py
@@ -10,7 +10,7 @@ from app import db
 from app.models import User, Post
 from app.translate import translation
 from app.main import bp
-from app.main.forms import EditProfileForm, PostForm
+from app.main.forms import EditProfileForm, PostForm, SearchForm
 
 
 @bp.route('/', methods=['GET', 'POST'])
@@ -150,13 +150,35 @@ def translate():
                                         request.form['dest_lang'])})
 
 
-@bp.before_request
+@bp.route('/search')
+@login_required
+def search():
+    """View function to handle search requests"""
+
+    if not g.search_form.validate():
+        return redirect(url_for('main.explore'))
+
+    page = request.args.get('page', 1, type=int)
+    posts, total = Post.search(g.search_form.q.data, page,
+                               current_app.config['POSTS_PER_PAGE'])
+
+    next_url = url_for('main.search', q=g.search_form.q.data, page=(page + 1)) \
+        if total > page * current_app.config['POSTS_PER_PAGE'] else None
+    prev_url = url_for('main.search', q=g.search_form.q.data, page=(page - 1)) \
+        if (page - 1) > 0 else None
+    return render_template('index.html', title='Search Results', posts=posts,
+                           next_url=next_url, prev_url=prev_url,
+                           user=current_user, search_results=True)
+
+
+@bp.before_app_request
 def before_request():
     """Actions to take before each request"""
 
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
+        g.search_form = SearchForm()
 
     # Register flask babel locale with flask g?
     g.locale = str(get_locale())
